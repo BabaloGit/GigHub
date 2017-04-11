1f0a765a5997e02ee3a5b1a44d945a5bd52302ae
Babalo Cetywa <babalocetywa@gmail.com>
2017-04-11 15:21:24 +0200

    Restructure Application.

---
+++ GigHub/Core/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using GigHub.Core.Repositories;
+using GigHub.Persistence.Repositories;
+
+namespace GigHub.Core
+{
+    public interface IUnitOfWork
+    {
+        IGigRepository Gigs { get; }
+        IAttendanceRepository Attendances { get; }
+        IGenreRepository Genres { get; }
+        IFollowingRepository Followings { get; }
+        void Complete();
+    }
+}
