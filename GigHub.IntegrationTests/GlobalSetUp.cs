f40bacd5a11e4c30be08ef2e2e3f1023fecebb8c
Babalo Cetywa <babalocetywa@gmail.com>
2017-04-22 15:42:24 +0200

    Create Setup for Integration Database.

---
+++ GigHub.IntegrationTests/GlobalSetUp.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System.Data.Entity.Migrations;
+
+namespace GigHub.IntegrationTests
+{
+    [SetUpFixture]
+    public class GlobalSetUp
+    {
+        [SetUp]
+        public void Setup()
+        {
+            var configuration = new Migrations.Configuration();
+            var migrator = new DbMigrator(configuration);
+            migrator.Update();
+        }
+    }
+}
