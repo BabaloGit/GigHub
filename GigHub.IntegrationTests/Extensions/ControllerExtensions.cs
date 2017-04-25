402621e35ceacc0781e87f5ca129fef4e201783a
Babalo Cetywa <babalocetywa@gmail.com>
2017-04-25 12:10:33 +0200

    Mock Current User for Normal MVC Controllers.

---
+++ GigHub.IntegrationTests/Extensions/ControllerExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Moq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GigHub.IntegrationTests.Extensions
+{
+    public static class ControllerExtensions
+    {
+        public static void MockCurrentUser(this Controller controller, string userId, string userName)
+        {
+            var identity = new GenericIdentity(userName);
+            identity.AddClaim(
+                new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name", userName));
+            identity.AddClaim(
+                new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier", userId));
+
+            var principal = new GenericPrincipal(identity, null);
+
+            var mockContext = new Mock<HttpContextBase>();
+            mockContext.SetupGet(c => c.User).Returns(principal);
+
+            var mockControllerContext = new Mock<ControllerContext>();
+            mockControllerContext.SetupGet(c => c.HttpContext).Returns(mockContext.Object);
+
+            controller.ControllerContext = mockControllerContext.Object;
+        }
+    }
+}
